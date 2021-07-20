617c11d718021690a72b86c5a6115708c2840b89
ajensen415 <ajensen415@yahoo.com>
2021-07-20 10:59:43 -0400

    removed console.log test, corrected typo & fixed formatting

--- JS/scripts.js
+++ JS/scripts.js
@@ -43,32 +43,31 @@ function userCard(response) {
             <p class="card-text cap">${employee.location.city}, ${employee.location.state}</p>
         </div>`);
 
-        //event listenered that listens for a clicked employee & creates their individual modal. 
+        //event listener that listens for a clicked employee & creates their individual modal. 
         card.addEventListener('click', () => createModal(employee))
     };
 } 
 
 //function to create modal for clicked employee & add it to the HTML.
 function createModal(employee) {
-        console.log(employee);
-        let birthday = new Date(employee.dob.date);
+    let birthday = new Date(employee.dob.date);
         
-        document.body.insertAdjacentHTML('beforeend', `<div class="modal-container">
-        <div class="modal">
-            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
-                <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
-                <p class="modal-text">${employee.email}</p>
-                <p class="modal-text cap">${employee.location.city}, ${employee.location.state}</p>
-                <hr>
-                <p class="modal-text">${employee.phone}</p>
-                <p class="modal-text">${employee.location.street.number} ${employee.location.street.name} ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
-                <p class="modal-text">Birthday: ${birthday.getMonth()}/${birthday.getDay()}/${birthday.getYear()}</p>
-            </div>
-        </div>`);
+    document.body.insertAdjacentHTML('beforeend', `<div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="${employee.picture.large}" alt="profile picture">
+            <h3 id="name" class="modal-name cap">${employee.name.first} ${employee.name.last}</h3>
+            <p class="modal-text">${employee.email}</p>
+            <p class="modal-text cap">${employee.location.city}, ${employee.location.state}</p>
+            <hr>
+            <p class="modal-text">${employee.phone}</p>
+            <p class="modal-text">${employee.location.street.number} ${employee.location.street.name} ${employee.location.city}, ${employee.location.state} ${employee.location.postcode}</p>
+            <p class="modal-text">Birthday: ${birthday.getMonth()}/${birthday.getDay()}/${birthday.getYear()}</p>
+        </div>
+    </div>`);
         
-        //event listener that listens for a click on the close button to close out the open modal. 
-        let closeBtn = document.querySelector('.modal-close-btn');
-        closeBtn.addEventListener('click', () => closeBtn.parentElement.parentElement.remove());
+    //event listener that listens for a click on the close button to close out the open modal. 
+    let closeBtn = document.querySelector('.modal-close-btn');
+    closeBtn.addEventListener('click', () => closeBtn.parentElement.parentElement.remove());
 }
